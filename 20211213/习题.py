266321afef4c9eecb4a2442a55b444dd0d84a7ff
xwj <1983318249@qq.com>
2021-12-16 11:38:11 +0800

    python考前小复习

---
+++ 20211213/习题.py
@@ -0,0 +1,54 @@
+# str1 = "Runoob example....wow!!!"
+# str2 = "exam"
+# print(str1.find(str2,5,6))
+
+# def num(n):
+#     for i in n:
+#         print(i)
+#
+# num((2,3,4))
+
+# x = 1
+# y = 2
+# max = x>y?x:y
+
+
+# def func(a, b, c):
+#     lst = [a, b, c]
+#     lst.sort()
+#     print(lst[0], lst[1], lst[2])
+    # if a < b:
+    #     ret = a
+    #     a = b
+    #     b = ret
+    # if a < c:
+    #     ret = a
+    #     a = c
+    #     c = ret
+    # if b < c:
+    #     ret = b
+    #     b = c
+    #     c = ret
+    # print(c, b, a)
+
+
+# a = int(input("请输入整数a的值："))
+# b = int(input("请输入整数b的值："))
+# c = int(input("请输入整数c的值："))
+# func(a,b,c)
+
+# def greeting(args1, *tupleArgs, **dictArgs):
+#     print(args1)
+#     print(tupleArgs)
+#     print(dictArgs)
+# names = ['HTY', 'LFF', 'ZH']
+# info = {'schoolName' : 'NJRU', 'City' : 'Nanjing'}
+# greeting('Hello,', *names, **info)
+
+# print(list(range(1,10,3)))
+
+# print([x for x in [1,2,3,4,5] if x<3])
+
+if __name__ == "__main__":   # __name__的值就是__main__字符串
+    print(__name__)
+
